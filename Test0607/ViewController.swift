c0873071d011dbffe81ab2974a75ec5f7df79c77
kunren16 <kunren16@macbook11.local>
2016-06-07 10:11:25 +0900

    ０２

--- Test0607/ViewController.swift
+++ Test0607/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
 		// Do any additional setup after loading the view, typically from a nib.
 		
 		//保存するとMが表示される
+		
+		//ファイルを追加するとAが表示される
 	}
 
 	override func didReceiveMemoryWarning() {
